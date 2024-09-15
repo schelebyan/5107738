f1be9faaf148b6b8aaed69e5baaf1b4eaebd715d
Simon <schelebyan@cffhae.org>
2024-09-15 04:01:18 -0700

    fix error type

--- src/types/error.ts
+++ src/types/error.ts
@@ -1,9 +1,3 @@
-import { AxiosError } from "axios";
-
-export interface CustomAxiosError extends AxiosError {
-  message: string;
-}
-
 export enum PageError {
   Forbidden = "403",
   NotFound = "404",
